d7a8d39bf72c27a9e88b5f29c235a6e6894fc0eb
Najma <najmamosman@users.noreply.github.com>
2021-02-02 18:35:59 -0500

    Data cleaning/reduction + markdown edits
    - (mostly) completed data cleaning, removed all but 13 rows of the original 51, and filtered down to 631 objects from the original 4887.
    - updated markdown with exact rows/cols removed
    - new pdf knitted

--- scripts/00-data_cleaning.R
+++ scripts/00-data_cleaning.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Get and clean data on school info and student demographics from Ontario's Data Catalogue
 # Author: Najma Osman
-# Date: 26 January 2021
+# Date: 2 February 2021
 # Contact: naj.osman@mail.utoronto.ca / najmamosman@gmail.com
 # License: MIT
 # Pre-requisites: None
@@ -12,6 +12,7 @@
 library(here)
 library(janitor)
 library(tidyverse)
+library(readxl)
 library(curl)
 
 #### get and read in raw data #### 
@@ -33,18 +34,32 @@ raw_data <-
 cleaned_data <- 
   janitor::clean_names(raw_data)
 
-# save cleaned data set #
+# reduced data; #
+  # removing rows associated with:
+    # * containing elementary schools,
+    # * containing schools where the language is French, as they have different tests which are covered in a separate dataset,
+    # * having codes NA, NR, or ND in columns to do with Grade 9 EQAO and/or the Grade 10 literacy test (OSSLT),
+    # * containing code SP in columns to do with household income and/or percentage of students whose parents have some university education.
 
+filter_data <-
+  cleaned_data %>%
+  filter(school_level == "Secondary", school_language == "English") %>%
+  filter(percentage_of_students_whose_parents_have_some_university_education != "SP", 
+         percentage_of_school_aged_children_who_live_in_low_income_households != "SP") %>%
+  filter(change_in_grade_9_academic_mathematics_achievement_over_three_years != "NA", 
+         change_in_grade_9_academic_mathematics_achievement_over_three_years != "N/R", 
+         change_in_grade_9_academic_mathematics_achievement_over_three_years != "N/D") %>%
+  filter(change_in_grade_10_osslt_literacy_achievement_over_three_years != "NA", 
+         change_in_grade_10_osslt_literacy_achievement_over_three_years != "N/R", 
+         change_in_grade_10_osslt_literacy_achievement_over_three_years != "N/D")
+  
 
-#### reduced data; #### 
-# removing rows associated with
+  # remove the following columns/select all the others (probably easier): 
+      #  * location information: board number, board name, board type, school number, building suite, P.O. Box, Street, postal code, province, latitude, longitude,
+      # * school info: school type, school special condition code (e.g., Alternative, Adult, NA), grade range, enrolment, school language,
+      # * contact information: phone number, fax number, school website, board website
+      # * testing information: grade 3 and 6 results (EQAO, achievement of provincial standard (percentage of student and change over 3 years))
 
+=
 
-# remove the following columns: 
-  # - school number, building suite, P.O. Box, Street, postal code
-  # - phone number, fax number, school website, board website
-  # - extract date
-
-
-#### save reduced data set ####
-
+#### save cleaned/reduced data set ####
