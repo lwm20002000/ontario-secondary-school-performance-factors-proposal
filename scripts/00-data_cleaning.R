a36dcb97c2fc902f0f7c2db7be64d390d8c39aed
Najma <najmamosman@users.noreply.github.com>
2021-02-06 14:08:55 -0500

    Updated tidyverse row filters/column selections
    This was to remove the last of the N/A's, N/R's, N/D's,
    brought back enrolment and board name cols.

--- scripts/00-data_cleaning.R
+++ scripts/00-data_cleaning.R
@@ -18,11 +18,10 @@ library(curl)
 
 #### get and read in raw data #### 
   # Data from Ontario Data Catalogue (https://data.ontario.ca/dataset/school-information-and-student-demographics) #
-
 url <- 
   "https://data.ontario.ca/dataset/d85f68c5-fcb0-4b4d-aec5-3047db47dcd5/resource/602a5186-67f5-4faf-94f3-7c61ffc4719a/download/new_sif_data_table_2018_2019prelim_en_december.xlsx"
 destfile <- 
-  "inputs/data/new_sif_data_table_2018_2019prelim_en_december.xlsx"
+  here("inputs/data/new_sif_data_table_2018_2019prelim_en_december.xlsx")
 curl::curl_download(url, destfile)
 # read into R
 raw_data <- 
@@ -39,20 +38,29 @@ clean_names <-
   # removing rows associated with:
     # * containing elementary schools,
     # * containing schools where the language is French, as they have different tests which are covered in a separate dataset,
-    # * having codes NA, NR, or ND in columns to do with Grade 9 EQAO and/or the Grade 10 literacy test (OSSLT),
+    # * having codes NA, NR, or ND in columns to do with Grade 9 EQAO/mathematics achievement of provincial standard and/or the Grade 10 literacy test (OSSLT),
     # * containing code SP in columns to do with household income and/or percentage of students whose parents have some university education.
 
 filter_data <-
-  cleaned_data %>%
+  clean_names %>%
   filter(school_level == "Secondary", school_language == "English") %>%
   filter(percentage_of_students_whose_parents_have_some_university_education != "SP", 
          percentage_of_school_aged_children_who_live_in_low_income_households != "SP") %>%
   filter(change_in_grade_9_academic_mathematics_achievement_over_three_years != "NA", 
          change_in_grade_9_academic_mathematics_achievement_over_three_years != "N/R", 
          change_in_grade_9_academic_mathematics_achievement_over_three_years != "N/D") %>%
+  filter(change_in_grade_9_applied_mathematics_achievement_over_three_years != "NA", 
+         change_in_grade_9_applied_mathematics_achievement_over_three_years != "N/R", 
+         change_in_grade_9_applied_mathematics_achievement_over_three_years != "N/D") %>%
   filter(change_in_grade_10_osslt_literacy_achievement_over_three_years != "NA", 
          change_in_grade_10_osslt_literacy_achievement_over_three_years != "N/R", 
-         change_in_grade_10_osslt_literacy_achievement_over_three_years != "N/D")
+         change_in_grade_10_osslt_literacy_achievement_over_three_years != "N/D") %>%
+  filter(percentage_of_grade_9_students_achieving_the_provincial_standard_in_academic_mathematics != "NA", 
+         percentage_of_grade_9_students_achieving_the_provincial_standard_in_academic_mathematics != "N/R", 
+         percentage_of_grade_9_students_achieving_the_provincial_standard_in_academic_mathematics != "N/D") %>%
+  filter(percentage_of_grade_9_students_achieving_the_provincial_standard_in_applied_mathematics != "NA", 
+         percentage_of_grade_9_students_achieving_the_provincial_standard_in_applied_mathematics != "N/R", 
+         percentage_of_grade_9_students_achieving_the_provincial_standard_in_applied_mathematics != "N/D") 
   
 
   # remove the following columns/select all the others (probably easier): 
@@ -63,8 +71,8 @@ filter_data <-
 
 select_data <-
   filter_data %>%
-  select(-c(board_number, board_name, board_type, school_number, building_suite, p_o_box, street, postal_code, municipality, province, latitude, longitude,
-            school_type, school_special_condition_code, school_language, school_level, grade_range, enrolment,
+  select(-c(board_number, board_type, school_number, building_suite, p_o_box, street, postal_code, municipality, province, latitude, longitude,
+            school_type, school_special_condition_code, school_language, school_level, grade_range,
             phone_number, fax_number, school_website, board_website)) %>%
   select(-c(percentage_of_students_whose_first_language_is_not_english, percentage_of_students_whose_first_language_is_not_french, percentage_of_students_who_are_new_to_canada_from_a_non_english_speaking_country, percentage_of_students_who_are_new_to_canada_from_a_non_french_speaking_country)) %>%
   select(-c(change_in_grade_3_reading_achievement_over_three_years, change_in_grade_3_writing_achievement_over_three_years, change_in_grade_3_mathematics_achievement_over_three_years, 
@@ -76,4 +84,3 @@ select_data <-
 #### save cleaned/reduced data set ####
 
 writexl::write_xlsx(select_data, here("outputs/data/cleaned_data.xlsx"), col_names = TRUE)
-
